e7eb6e7f97c1dd2c7848c1404b98e3713d894f94
houssainiS <houssaini.slimen@gmail.com>
2025-01-16 21:33:56 +0100

    learning DTL

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'pages/index.html')
+    x={'name' : 'nana','age':25}
+    return render(request,'pages/index.html',x)
 
 def about(requeest):
     pass
