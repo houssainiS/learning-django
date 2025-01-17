c22f682657f9b714171d2fa2395c69ee4db10e13
houssainiS <houssaini.slimen@gmail.com>
2025-01-17 14:17:26 +0100

    added the statics codes in the settings.py & collected it using 'python manage.py collectstatic'
    it added new folders outside the project

--- project/settings.py
+++ project/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static') #added this
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'project/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
