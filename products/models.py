782196e52661b9d12f93e62a9c3d4175f95bee37
houssainiS <houssaini.slimen@gmail.com>
2025-01-20 20:28:46 +0100

    added a default variable to the models elements

--- products/models.py
+++ products/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    content = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='photos/%Y/%m/%d')
+    name = models.CharField(max_length=100 ,default='empety')
+    content = models.TextField(default='empety')
+    price = models.DecimalField(max_digits=10, decimal_places=2 , default=0)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/2025/01/19/flying_cat.jpg')
     active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.name
     
     class Meta:
-        ordering = ['-price']
+        ordering = ['name']
