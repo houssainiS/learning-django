d6ad773f459f43ccc2f0093efa6a0254f2cf8af6
houssainiS <houssaini.slimen@gmail.com>
2025-01-20 20:32:00 +0100

    changed the TextField that I can leave it empety

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100 ,default='empety')
-    content = models.TextField(default='empety')
+    content = models.TextField(null=True , blank=True) #it meanse i can save it empety
     price = models.DecimalField(max_digits=10, decimal_places=2 , default=0)
     image = models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/2025/01/19/flying_cat.jpg')
     active = models.BooleanField(default=True)
