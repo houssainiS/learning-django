ca4901753d516837a7f90b83593bc97e108cb5a4
houssainiS <houssaini.slimen@gmail.com>
2025-01-20 20:33:55 +0100

    changed the name of the tables elements just in the admin page

--- products/models.py
+++ products/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=100 ,default='empety')
-    content = models.TextField(null=True , blank=True) #it meanse i can save it empety
+    name = models.CharField(max_length=100 ,default='empety' , verbose_name='Title')  #CHANGED THE NAME TO TITLE verbose_name='Title'
+    content = models.TextField(null=True , blank=True) 
     price = models.DecimalField(max_digits=10, decimal_places=2 , default=0)
     image = models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/2025/01/19/flying_cat.jpg')
     active = models.BooleanField(default=True)
