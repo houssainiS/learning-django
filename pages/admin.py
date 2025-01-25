eccca4319f197e918ad8bf7c36754c118aca6b13
houssainiS <houssaini.slimen@gmail.com>
2025-01-25 14:02:36 +0100

    learning relations between tables learned 'OneToneField'

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Female, Male
 # Register your models here.
+
+admin.site.register(Female)
+
+admin.site.register(Male)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Female(models.Model):
+    name_female = models.CharField(max_length=100,null=True, blank=True)
+    def  __str__(self):
+        return self.name_female
+
+class Male(models.Model):
+    name_male = models.CharField(max_length=100,null=True, blank=True)
+    girls = models.OneToOneField(Female, on_delete=models.PROTECT)
+
+    def  __str__(self):
+        return self.name_male
