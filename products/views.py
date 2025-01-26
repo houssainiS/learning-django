fe4bb96af79b9e49dbfcd0e0a00b4793bcab4eea
houssainiS <houssaini.slimen@gmail.com>
2025-01-26 15:47:41 +0100

    learned query set by get , filter and all

--- products/views.py
+++ products/views.py
@@ -6,4 +6,8 @@ def product(request):
     return render(request, 'products/product.html')
 
 def products(request):
-    return render(request, 'products/products.html', {'pro':Product.objects.all()})
+    # x = {'pro':Product.objects.all()} #all the products from the database are passed to the products.html template for display
+    # x = {'pro':Product.object.get(id=1)} #one product is passed to the products.html template for display with id=1
+    pro = Product.objects.all()
+    x = {'pro':pro.filter(id=1)}  #one product is passed to the products.html template for display with id=1
+    return render(request, 'products/products.html', x)
