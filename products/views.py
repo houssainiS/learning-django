b62f579eda6dfd9d2988d12477db0881201d82cd
houssainiS <houssaini.slimen@gmail.com>
2025-01-26 16:05:04 +0100

    made some improvments in design

--- products/views.py
+++ products/views.py
@@ -9,5 +9,6 @@ def products(request):
     # x = {'pro':Product.objects.all()} #all the products from the database are passed to the products.html template for display
     # x = {'pro':Product.object.get(id=1)} #one product is passed to the products.html template for display with id=1
     pro = Product.objects.all()
-    x = {'pro':pro.filter(id=1)}  #one product is passed to the products.html template for display with id=1
+    #x = {'pro':pro.filter(id=1)}  #one product is passed to the products.html template for display with id=1
+    x = {'pro':pro}  #all the products from the database are passed to the products.html template for display
     return render(request, 'products/products.html', x)
