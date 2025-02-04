1cca56faa8f9a4624a30d8c48764088f0e74824f
houssainiS <houssaini.slimen@gmail.com>
2025-02-04 13:36:16 +0100

    learned to filter in range and in and others

--- products/views.py
+++ products/views.py
@@ -16,6 +16,14 @@ def products(request):
     #x = {'pro':pro.order_by('price')} #all the products  ordered by price
     #x = {'pro':str(pro.count())} #the number of products in the database is passed to the products.html template for display
     #x = {'pro':pro.exclude(price=150)} #all the products  excluding products with price=150
-    x = {'pro':pro} 
+    ######################################
+    #x = {'pro':Product.objects.filter(price__gt=100)} #all the products with price greater than 100
+    #x = {'pro':Product.objects.filter(price__lt=100)} #all the products with price less than 100
+    #x = {'pro':Product.objects.filter(price__range=(100, 200))} #all the products with price between 100 and 200
+    #x = {'pro':Product.objects.filter(price__in=[100, 200, 300])} #all the products with price 100, 200, or 300
+    #x = {'pro':Product.objects.filter(price__isnull=True)} #all the products with price is null
+    #x = {'pro':pro.filter(price_exact=10)}  #all the products with price exactly 10
+    #x = {'pro':pro.filter(name__contains='p')} #all the products with name containing 'p' 
+    x = {'pro':pro}
     return render(request, 'products/products.html', x)
 
