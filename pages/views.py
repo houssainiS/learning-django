499e4bf73e37fe1a5cad2fba04df539902e34ad2
houssainiS <houssaini.slimen@gmail.com>
2025-02-06 23:21:39 +0100

    the right way to make a form and save it in django
    no hackers or problems

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Login
 from .forms import LoginForm
 # Create your views here.
@@ -8,11 +8,17 @@ def index(request):
     return render(request,'pages/index.html',x)
 
 def about(request):
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    if username:  # You could also check for password if needed
-            data = Login(username=username, password=password)
-            data.save()
+    if request.method == 'POST':  # this condition checks if the form was submitted
+
+        form = LoginForm(request.POST)
+        # check if the form is valid  # checks if all the required fields in the form are filled correctly
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            if username:  # You could also check for password if needed
+                data = Login(username=username, password=password)
+                data.save()
+                return redirect('about') # needed to fix the reload thing after form submission
             
 
     return render(request,'pages/about.html',{'lf':LoginForm}) #we pass the form to the about.html template 
