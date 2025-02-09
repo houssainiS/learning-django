b8233dc9145d947b22e5a1680446c72e915ecca6
houssainiS <houssaini.slimen@gmail.com>
2025-02-09 15:04:21 +0100

    changing the tables display in admin pannel now shows (id , name , price)

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Product ,Test
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name','price','catigory') #display the id, name, price and catigory fields in the admin list view
+    list_display_links =('id','name') #make the id and name fields clickable in the admin list view
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Test)
 admin.site.site_header = 'SI Houssaini'
 admin.site.site_title = 'SI Houssaini Admin Panel'
